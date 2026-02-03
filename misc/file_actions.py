d82cef65c1a7468a3216a93ed94c8f1686a9a5a8
VirgushAA <vaa2024@ya.ru>
2026-02-03 13:22:58 +0700

    file save\load

---
+++ misc/file_actions.py
@@ -0,0 +1,20 @@
+from maze.maze import Maze
+
+
+def save_to_file(filename: str, maze: Maze):
+    """
+    Saves the given maze to a file
+    :param filename: name of the file
+    :param maze: maze to save
+    :return: None
+    """
+
+    try:
+        with open(filename, 'w') as f:
+
+def load_from_file(filename: str):
+    """
+
+    :param filename:
+    :return:
+    """
